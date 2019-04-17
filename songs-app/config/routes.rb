66484357265fc8f4773991edd66c90819c7db475
Meshal505 <rings-3@hotmail.com>
2019-04-17 15:38:15 +0300

    almost done

--- songs-app/config/routes.rb
+++ songs-app/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+  # get 'albums/new'
+  # get 'albums/create'
+  # get 'albums/update'
+  # get 'albums/edit'
+  # get 'albums/show'
+    resources :albums
+  root 'albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
