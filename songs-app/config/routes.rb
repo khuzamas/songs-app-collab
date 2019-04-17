950fe8d7b01e52393035805d72a628b6cc1942ad
alhanoufoth33 <alhanoufoth33@gmail.com>
2019-04-17 15:37:02 +0300

    first commit

--- songs-app/config/routes.rb
+++ songs-app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'artists/index'
+  # get 'artists/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "artist#index"
+  resources :artists
+
 end
